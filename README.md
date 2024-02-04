5e1db7260f3fbe741972283784836be0d2e25425
Milan.Cieliebak <milan1111@gmx.de>
2024-02-04 14:55:47 +0100

    Added embedding function

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # embedding
+
 Build with Python 3.11 virtual environment
+
+# Installation
+
+``python3 -m venv venv``
+
+``llm install llm-sentence-transformers``
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import llm, sqlite3
+
+def setup():
+    con=sqlite3.connect("embedding.db")
+    cur=con.cursor()
+    cur.execute("create table embedding(file, score)")
+    print(cur.execute("select * from embedding").fetchall())
+    cur.close()
+    con.close()
+
+def embed(file_name: str) -> None:
+    embedding_model = llm.get_embedding_model("sentence-transformers/all-MiniLM-L6-v2")
+    file = open(file_name)
+    vector = embedding_model.embed(file.read())
+    file.close()
+    con = sqlite3.connect("embedding.db")
+    cur = con.cursor()
+    insert="insert into embedding values {}".format(",".join(map(lambda x: "('"+file_name+"',"+str(x)+")",vector)))
+    cur.execute(insert)
+    print(cur.execute("select * from embedding").fetchall())
+    cur.close()
+    con.close()
+
 def main():
-    pass
+    import subprocess
+    if subprocess.run(["ls", "embedding.db"],capture_output=True).returncode!=0:
+        setup()
+    embed("data/equalstreetnames.md")
+    subprocess.run(["rm","embedding.db"])
 
 if __name__ == "__main__":
     main()
