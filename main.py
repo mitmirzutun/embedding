665c3b69db703f7cdbb97e61072cded14d877946
Milan.Cieliebak <milan1111@gmx.de>
2024-02-08 11:53:55 +0100

    Reformatted files

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-import llm, sqlite3, subprocess, json, collections.abc, os.path
+import collections.abc
+import json
+import llm
+import os.path
+import sqlite3
+import subprocess
+EMBEDDING_DB="embedding.db"
 
-def cosine_similarity(a,b):
+def cosine_similarity(a, b):
     dot_product = sum(x * y for x, y in zip(a, b))
     magnitude_a = sum(x * x for x in a) ** 0.5
     magnitude_b = sum(x * x for x in b) ** 0.5
     return dot_product / (magnitude_a * magnitude_b)
 
+
 def setup():
-    con=sqlite3.connect("embedding.db")
-    cur=con.cursor()
+    con = sqlite3.connect(EMBEDDING_DB)
+    cur = con.cursor()
     cur.execute("create table embedding(file, score)")
     cur.close()
     con.close()
 
+
 def embed(file_name: str) -> None:
-    if "'"  in file_name:
+    if "'" in file_name:
         raise ValueError("file name is a SQL Injection attempt")
-    if subprocess.run(["ls", "embedding.db"],capture_output=True).returncode!=0:
+    if subprocess.run(["ls", EMBEDDING_DB], capture_output=True).returncode != 0:
         setup()
-    file_name=os.path.relpath(file_name)
-    con = sqlite3.connect("embedding.db")
+    file_name = os.path.relpath(file_name)
+    con = sqlite3.connect(EMBEDDING_DB)
     cur = con.cursor()
-    if cur.execute(f"select 1 from embedding where file='{file_name}'").fetchone()!=None:
+    if cur.execute(f"select 1 from embedding where file='{file_name}'").fetchone() is not None:
         cur.close()
         con.close()
         return
@@ -29,47 +37,58 @@ def embed(file_name: str) -> None:
     file = open(file_name)
     vector = embedding_model.embed(file.read())
     file.close()
-    insert="insert into embedding values ('"+file_name+"','"+json.dumps(vector)+"')"
+    insert = "insert into embedding values ('"+file_name+"','"+json.dumps(vector)+"')"
     cur.execute(insert)
     con.commit()
     cur.close()
     con.close()
 
+
 def grep_all_files(*paths: str) -> collections.abc.Iterator[str]:
-    #print(paths)
-    if len(paths)==0:
+    if len(paths) == 0:
         return
-    if len(paths)!=1:
+    if len(paths) != 1:
         for path in paths:
             yield from grep_all_files(path)
-    path=os.path.abspath(paths[0])
+    path = os.path.abspath(paths[0])
     if os.path.isdir(path):
-        ls_result=subprocess.run(["ls",path],capture_output=True).stdout.decode("utf8").strip().split("\n")
+        ls_result = subprocess.run(["ls", path], capture_output=True).stdout.decode("utf8").strip().split("\n")
         for rel_path in ls_result:
-            yield from grep_all_files(os.path.join(path,rel_path))
+            yield from grep_all_files(os.path.join(path, rel_path))
     if os.path.isfile(path):
         yield path
 
+
 def plot() -> None:
     import matplotlib.pyplot
-    figure, axes= matplotlib.pyplot.subplots()
-    data=[]
-    con = sqlite3.connect("embedding.db")
+    data = []
+    con = sqlite3.connect(EMBEDDING_DB)
     cur = con.cursor()
-    result = cur.execute("select e1.file, e1.score, e2.file, e2.score from embedding e1 cross join embedding e2 where e1.file!=e2.file")
+    magnitude_x = []
+    magnitude_y = []
+    result = cur.execute("select e1.file, e1.score, e2.file, e2.score from embedding e1" +
+                         " cross join embedding e2 where e1.file!=e2.file")
     for (file1, score1, file2, score2) in result:
-        data.append(cosine_similarity(json.loads(score1),json.loads(score2)))
+        array1 = json.loads(score1)
+        array2 = json.loads(score2)
+        data.append(cosine_similarity(array1, array2))
+        magnitude_x.append(sum(x*x for x in array1))
+        magnitude_y.append(sum(x*x for x in array2))
     cur.close()
     con.close()
-    axes.scatter(range(len(data)),data)
+    _figure, axes = matplotlib.pyplot.subplots(2, 2)
+    axes[0, 0].scatter(range(len(data)), data)
+    axes[0, 1].scatter(magnitude_x, magnitude_y)
     matplotlib.pyplot.savefig("embedding_visualized.png")
     matplotlib.pyplot.show()
 
+
 def main():
-    for path in grep_all_files("data","scraping/textfiles"):
+    for path in grep_all_files("data", "scraping/textfiles"):
         embed(path)
     plot()
     # subprocess.run(["rm","embedding.db"])
+
 
 if __name__ == "__main__":
     main()
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -9,19 +9,21 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 def get_last_url_part(url: str):
     url_path = Path(urllib.parse.urlparse(url).path)
     return url_path.name
-    
-def wiki_to_txt(infile) -> None:
+
+
+def wiki_to_txt(_infile) -> None:
     with open("wikipedialist.csv") as infile:
         wikiurls = infile.read().split("\n")
 
     for url in wikiurls:
         if len(url) > 0:
-            response = requests.get( url=url )
+            response = requests.get(url=url)
             soup = BeautifulSoup(response.content, 'html.parser')
             outfile = get_last_url_part(url) + ".txt"
-            with open(os.path.join(dir_path,"textfiles",outfile), "w") as of:
+            with open(os.path.join(dir_path, "textfiles", outfile), "w") as of:
                 of.write(soup.get_text())
+
 
 if __name__ == "__main__":
     print(get_last_url_part("https://thewalrus.ca/the-ethnic-vote-is-a-myth"))
-    wiki_to_txt(os.path.join(dir_path,"wikipedialist.csv"))
+    wiki_to_txt(os.path.join(dir_path, "wikipedialist.csv"))
